1e3795a3a48e6bf84ce6f795ebaca7ebdf334827
rosalomon <robin.salomonsson@gmail.com>
2024-05-07 12:05:50 +0200

    Inital

---
+++ räknesnurra/script.js
@@ -0,0 +1,11 @@
+function calculate() {
+    var loanAmount = document.getElementById('loanAmount').value;
+    var interestRate = document.getElementById('interestRate').value;
+    var currentInterestCost = loanAmount * (interestRate / 100);
+    var newInterestRate = interestRate - 0.25;
+    var newInterestCost = loanAmount * (newInterestRate / 100);
+    var difference = currentInterestCost - newInterestCost;
+    document.getElementById('output').innerHTML = 'Nuvarande räntekostnad: ' + currentInterestCost.toFixed(2) + '<br>' +
+                                                   'Ny räntekostnad vid 0.25 procentenheter lägre ränta: ' + newInterestCost.toFixed(2) + '<br>' +
+                                                   'Skillnad: ' + difference.toFixed(2);
+}
