3b0815e5d82d379b2d3eb5f6ede18e9bcaee7080
rosalomon <robin.salomonsson@gmail.com>
2024-05-22 12:21:46 +0200

    Ändrad logik och sajt

--- rentbuy/script.js
+++ rentbuy/script.js
@@ -16,11 +16,7 @@ document.querySelectorAll('.year-btn').forEach(button => {
     button.addEventListener('click', function() {
         toggleActiveYearButton(this);
         const years = parseInt(this.getAttribute('data-years'));
-        if (years === 25) {
-            showAverageReturnRateSliders(5, 25);
-        } else {
-            showReturnRateSliders(years);
-        }
+        showReturnRateSliders(years);
     });
 });
 
@@ -105,37 +101,6 @@ function showReturnRateSliders(years) {
     returnRatesDiv.classList.remove('hidden');
 }
 
-function showAverageReturnRateSliders(interval, totalYears) {
-    const returnRatesDiv = document.getElementById('returnRates');
-    returnRatesDiv.innerHTML = '';
-    for (let i = 0; i < totalYears; i += interval) {
-        const label = document.createElement('label');
-        label.textContent = `Snittavkastning år ${i}-${i + interval}:`;
-
-        const slider = document.createElement('input');
-        slider.type = 'range';
-        slider.min = '0';
-        slider.max = '20';
-        slider.step = '0.1';
-        slider.id = `averageReturnRate${i}`;
-        slider.name = `averageReturnRate${i}`;
-
-        const span = document.createElement('span');
-        span.id = `averageReturnRateValue${i}`;
-        span.textContent = '0';
-
-        slider.addEventListener('input', function() {
-            span.textContent = this.value;
-        });
-
-        returnRatesDiv.appendChild(label);
-        returnRatesDiv.appendChild(slider);
-        returnRatesDiv.appendChild(span);
-        returnRatesDiv.appendChild(document.createElement('br'));
-    }
-    returnRatesDiv.classList.remove('hidden');
-}
-
 function calculateBuyOption() {
     const purchasePrice = parseFloat(document.getElementById('purchasePrice').value);
     const monthlyFee = parseFloat(document.getElementById('monthlyFee').value);
@@ -151,16 +116,9 @@ function calculateBuyOption() {
     }
 
     let futureValueBuy = purchasePrice;
-    if (years === 25) {
-        for (let i = 0; i < years; i += 5) {
-            const averageReturnRate = parseFloat(document.getElementById(`averageReturnRate${i}`).value) / 100;
-            futureValueBuy *= Math.pow((1 + averageReturnRate), 5);
-        }
-    } else {
-        for (let i = 1; i <= years; i++) {
-            const returnRate = parseFloat(document.getElementById(`returnRate${i}`).value) / 100;
-            futureValueBuy *= (1 + returnRate);
-        }
+    for (let i = 1; i <= years; i++) {
+        const returnRate = parseFloat(document.getElementById(`returnRate${i}`).value) / 100;
+        futureValueBuy *= (1 + returnRate);
     }
 
     document.getElementById('buyResult').textContent = `Framtida värde: ${futureValueBuy.toFixed(2)} kr`;
